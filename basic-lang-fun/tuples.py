b826ddc1b124052ad111de67b8e39f564260ac17
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-12 14:00:07 -0300

    .

---
+++ basic-lang-fun/tuples.py
@@ -0,0 +1,9 @@
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+print(green)
+print(yellow)
+print(red)
