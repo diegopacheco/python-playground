870e75844d92dad5d32c52d0309745cbc2fa197e
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-12 13:36:45 -0300

    conditionals

---
+++ basic-lang-fun/ifelse.py
@@ -0,0 +1,20 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+b = 200
+if b > a: print("*** b is greater than a ***")
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
