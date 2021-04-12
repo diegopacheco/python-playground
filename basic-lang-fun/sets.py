5dca52eb538d0522149f8d9ac9eb8f81d477e6f2
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-12 14:01:36 -0300

    sets

---
+++ basic-lang-fun/sets.py
@@ -0,0 +1,14 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
