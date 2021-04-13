342286e599229584a2fce74e75d908b6e7666dee
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-12 22:53:18 -0300

    create python-3 project script added

---
+++ create-python3-project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir src/
+touch src/main.py
+
+echo "print('Hello, world!')" > src/main.py
+echo "numpy" > requirements.txt 
+
+echo "#!/bin/bash 
+pip install -r requirements.txt" > install-deps.sh 
+chmod +x install-deps.sh
+
+touch run.sh
+echo "#!/bin/bash
+
+python3 src/main.py"
+chmod +x run.sh
