13fa2d0c73f3ebca7235b5ff40aad5e4a37b8f5b
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-12 21:35:13 -0300

    disct

---
+++ basic-lang-fun/dicts.py
@@ -0,0 +1,13 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
