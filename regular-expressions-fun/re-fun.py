aff4878ea778f00ef237a4df2ce06739132b5584
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-12 22:08:20 -0300

    regeex

---
+++ regular-expressions-fun/re-fun.py
@@ -0,0 +1,17 @@
+import re
+
+txt = "The rain in Spain"
+x = re.findall("^The.*Spain$", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.search("\s", txt)
+print("The first white-space character is located in position:", x.start())
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
