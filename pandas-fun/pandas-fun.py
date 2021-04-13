b96c9faa9f8297940565dc9e4028c959943898df
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-12 22:19:11 -0300

    pandas

---
+++ pandas-fun/pandas-fun.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+calories = {"day1": 420, "day2": 380, "day3": 390}
+myvar = pd.Series(calories)
+print(myvar)
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+#load data into a DataFrame object:
+df = pd.DataFrame(data)
+print(df) 
