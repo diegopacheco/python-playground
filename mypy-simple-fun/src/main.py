a16ee22d9d606a8cfb0f130af60f553624bd3260
diegopacheco <diego.pacheco.it@gmail.com>
2022-03-30 00:44:30 -0700

    mypy

---
+++ mypy-simple-fun/src/main.py
@@ -0,0 +1,10 @@
+import itertools
+from typing import Iterator
+
+def fib(n: int) -> Iterator[int]:
+    a, b = 0, 1
+    while a < n:
+        yield a
+        a, b = b, a+b
+
+print(list(fib(10)))
