20ca8d93407f55b93868397ee1809787d603b7a6
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-18 02:21:25 -0700

    simple counters

--- daemon-thread-python-fun/src/main.py
+++ daemon-thread-python-fun/src/main.py
@@ -1,26 +1,46 @@
 import json
 
-class MetricsManager:
+class MetricsManager(object):
     counters = {
         'ok': 0,
         'error': 0
     }
 
-    def registerSucess():
-        counters["ok"] += 1
+    def registerSucess(self):
+        self.counters["ok"] += 1
+        return True
 
-    def registerError():
-        counters["error"] += 1
+    def registerError(self):
+        self.counters["error"] += 1
+        return True
 
-    def get_stats():
-        result = json.dumps(counters, indent = 4)
+    def get_stats(self):
+        result = json.dumps(self.counters, indent = 4)
+        return result
+
+class ObservableMath(object):
+    
+    def __init__(self,metrics_manager):
+        self.metrics_manager = metrics_manager
+
+    def sum(self,a,b):
+        result = a + b
+        self.metrics_manager.registerSucess()
         return result
 
 def main():
     mm = MetricsManager()
-    mm.registerSucess
-    mm.registerSucess
-    print("Result is: " + str(mm.get_stats))
+    math = ObservableMath(mm)
+    math.sum(1,1)
+    math.sum(2,3)
+    math.sum(4,4)
+    math.sum(10,34)
+    math.sum(56,35)
+    math.sum(5645,365)
+    math.sum(546,335)
+
+    result = mm.get_stats()
+    print("Observability is: " + result)
 
 if __name__ == "__main__":
     main()
