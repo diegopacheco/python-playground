9eb10ce61ab27482f2ed5f936f24908c10dd8e46
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-19 19:46:25 -0700

    dynamic jq script

--- python-pika-rabbitmq-simple/get_dynamic_observability.sh
+++ python-pika-rabbitmq-simple/get_dynamic_observability.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#| jq '.[] |[.consumers, .name, .messages_ready]'
-
 curl -s -u guest:guest \
 -H "content-type:application/json" \
 http://127.0.0.1:15672/api/queues \
-| jq '{ queues: map(.name), messages: map(.messages_ready), consumers: map(.consumers) }'
+| jq '{ messages: map(.name, .messages_ready), consumers: map(.name, .consumers) }'
