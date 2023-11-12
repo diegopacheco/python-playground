c7875cf4f41c56f6df2b4a87acfd76540bbba883
diegopacheco <diego.pacheco.it@gmail.com>
2023-11-12 00:37:12 -0800

    celery + flask + redis

--- celery-hello/app.py
+++ celery-hello/app.py
@@ -1,14 +1,21 @@
+from flask import Flask, jsonify
 from celery import Celery
 
-app = Celery('hello', broker='redis://localhost:6379/0')
-app.conf.result_backend_transport_options = {
+app = Flask(__name__)
+app.config["CELERY_BROKER_URL"] = "redis://localhost:6379/0"
+
+celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
+celery.conf.update(app.config)
+celery.conf.result_backend_transport_options = {
     'global_keyprefix': 'celery_'
 }
 
-@app.task
-def hello():
-    return 'hello world'
+@celery.task()
+def add(x, y):
+        return x + y
 
-if __name__ == '__main__':
-    args = ['worker', '--loglevel=INFO']
-    app.worker_main(argv=args)
+@app.route('/')
+def add_task():
+    for i in range(10_000):
+        add.delay(i, i)
+    return jsonify({'status': 'ok'})
