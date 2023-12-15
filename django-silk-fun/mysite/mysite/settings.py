a471d1a99a655a60e86977aef08c8374a1e8f7b8
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-15 01:05:56 -0800

    django + silk added health checker

--- django-silk-fun/mysite/mysite/settings.py
+++ django-silk-fun/mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'silk'
+    'silk',
+    'health_check',
 ]
 
 MIDDLEWARE = [
