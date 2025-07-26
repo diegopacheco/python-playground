5cb1febeb2541db8ea2d6c8d63588318df8469d4
diegopacheco <diego.pacheco.it@gmail.com>
2025-07-26 00:10:01 -0700

    added iterator

---
+++ iterator/src/main.py
@@ -0,0 +1,15 @@
+class Countdown:
+    def __init__(self, start):
+        self.start = start
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.start <= 0:
+            raise StopIteration
+        self.start -= 1
+        return self.start + 1
+
+for num in Countdown(3):
+    print(num) 
