dd152264e00035c577d187f4bb044b75813e4e24
diegopacheco <diego.pacheco.it@gmail.com>
2026-07-16 13:06:34 -0700

    added control flow

--- bag-of-cats/control-flow.py
+++ bag-of-cats/control-flow.py
@@ -1,3 +1,12 @@
 age = 29
 status = "adult" if age >= 21 else "minor"
 print(f"you are {status}")
+
+for i in range(1, 6):
+    print(i)
+
+for index, value in enumerate(["a", "b", "c"]):
+    print(f"index: {index} value: {value}")
+
+evens = [x for x in range(10) if x % 2 == 0]
+print(f"even numbers 0 to 9 {evens}")
