4fac8d96eee6a1588d41a86bff8733a93254dc2d
diegopacheco <diego.pacheco.it@gmail.com>
2026-07-16 13:16:38 -0700

    added .

---
+++ bag-of-cats/dicts.py
@@ -0,0 +1,9 @@
+capitals = {
+    "Brazil": "Brazilia",
+    "Usa": "DC",
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Italy": "Rome",
+    "Argentina": "Buenos Aires",
+}
+print(capitals)
