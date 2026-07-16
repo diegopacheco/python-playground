10d2c3497937fe6482daad686999f138baf4112f
diegopacheco <diego.pacheco.it@gmail.com>
2026-07-16 13:13:58 -0700

    added .

--- bag-of-cats/lists.py
+++ bag-of-cats/lists.py
@@ -1,2 +1,8 @@
 res = sorted([1, 2, 3, 4, 5, 6], reverse=True)
 print(res)
+
+rev = res[::-1]
+print(rev)
+
+squares = [x**2 for x in range(5)]
+print(f"original: {[0, 1, 2, 3, 4]} squares: {squares}")
